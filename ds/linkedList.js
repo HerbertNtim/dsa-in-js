33c5e6875569ac36d00486ee79554e0bdb6b1f40
HerbertNtim <herbertntim15@gmail.com>
2025-06-17 05:56:07 +0000

    implemented singly linked list

--- ds/linkedList.js
+++ ds/linkedList.js
@@ -1,29 +1,92 @@
 // 10 ---> 5 ---> 16
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
-    }
+      next: null,
+    };
     this.tail = this.head;
-    this.length = 1
+    this.length = 1;
   }
 
   append(value) {
-    const newNode = {
-      value: value,
-      next: null
-    } 
+    const newNode = new Node(value);
     this.tail.next = newNode;
-    this.tail.value = newNode;
-    this.length++
+    this.tail = newNode;
+    this.length++;
     return this;
- }
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if(index >= this.length) {
+      return this.append(value)
+    }
+    const newNode = new Node(value)
+    const leader = this.traverseList(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++
+    return this
+  }
+
+  traverseList(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index) {
+      currentNode = currentNode.next;
+      counter++
+    }
+    return currentNode;
+  }
+
+  remove(index) {
+    if(index > this.length) {
+      return this;
+    }
+    const leader = this.traverseList(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--
+    return this;
+  }
 }
 
-const myList = new LinkedList(10)
+const myList = new LinkedList(10);
 
-myList.append(16)
+myList.append(5);
+myList.append(16);
 
-console.log(myList)
+myList.prepend(1);
+
+myList.insert(2, 99)
+myList.insert(20, 88)
+
+myList.remove(2)
+
+console.log(myList.printList());
